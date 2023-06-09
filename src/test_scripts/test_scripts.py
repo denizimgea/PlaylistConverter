628dec92c0245ae6591fbeb6bbe25e66b568dce8
Deniz İmge Avcı <denizimgeavci@gmail.com>
2023-06-09 16:52:54 +0200

    Implemented the necessary interaction with the Spotify API

--- src/test_scripts/test_scripts.py
+++ src/test_scripts/test_scripts.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def get_api_key(client_id : str, client_secret : str) -> dict:
+def get_api_key(client_id: str, client_secret: str) -> dict:
     spotify_url = "https://accounts.spotify.com/api/token"
     request_header = {"Content-Type": "application/x-www-form-urlencoded"}
     request_params = {"grant_type": "client_credentials",
@@ -10,3 +10,33 @@ def get_api_key(client_id : str, client_secret : str) -> dict:
                       "client_secret": client_secret}
     api_request = requests.post(spotify_url, headers=request_header, params=request_params)
     return json.loads(api_request.text)
+
+
+def get_playlist_information(api_key: dict, playlist_uri: str) -> dict:
+    spotify_url = f"https://api.spotify.com/v1/playlists/{playlist_uri}"
+    request_header = {"Authorization": str.format("{}  {}", api_key["token_type"],
+                                                  api_key["access_token"])}
+    api_request = requests.get(spotify_url, headers=request_header)
+    return json.loads(api_request.text)
+
+
+def get_playlist_tracks(api_key: dict, playlist_uri: str) -> list:
+    spotify_url = f"https://api.spotify.com/v1/playlists/{playlist_uri}"
+    request_header = {"Authorization": str.format("{}  {}", api_key["token_type"],
+                                                  api_key["access_token"])}
+    tracks = []
+    api_request = requests.get(spotify_url, headers=request_header)
+    playlist_information = json.loads(api_request.text)
+    playlist_tracks = playlist_information["tracks"]
+    while True:
+        tracks = tracks + playlist_tracks["items"]
+        if playlist_tracks["next"] is None:
+            break
+        api_request = requests.get(playlist_tracks["next"], headers=request_header)
+        playlist_tracks = json.loads(api_request.text)
+    return tracks
+
+
+def reduce_track_information(track_information: dict) -> str:
+    return str.format("{} {}", track_information["track"]["artists"][0]["name"],
+                      track_information["track"]["name"])
